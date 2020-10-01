93941f450b9f84acdd2e4c1cd70c1eea6d971339
Tanish1107 <72192376+Tanish1107@users.noreply.github.com>
2020-10-01 17:55:12 +0530

    Update Armstrong.py

--- Armstrong.py
+++ Armstrong.py
@@ -3,8 +3,8 @@ sum = 0
 temp = num
 while temp > 0:
    digit = temp % 10
-   sum += digit ** 3
-   temp //= 10
+   sum = sum + digit ** 3
+   temp =temp// 10
 if num == sum:
    print(num,"is an Armstrong number")
 else:
